337c74561be4d2c7fc10f295c6356a030fce0c05
AP <arjavprasad@gmail.com>
2022-05-26 15:02:19 -0500

    added conditional to ninjas list, will only display ninjas over 20 yrs old

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,16 +1,34 @@
 import React from 'react';
 
 const Ninjas = ({ninjas}) => {
+	/*
 	const ninjaList = ninjas.map(ninja => {
-		return (
-			<div className="ninja" key={ninja.id}>
-				<div>Name: {ninja.name}</div>
-				<div>Age: {ninja.age}</div>
-				<div>Belt: {ninja.belt}</div>
-			</div>
-		);
+		if (ninja.age > 20) {
+			return (
+				<div className="ninja" key={ninja.id}>
+					<div>Name: {ninja.name}</div>
+					<div>Age: {ninja.age}</div>
+					<div>Belt: {ninja.belt}</div>
+				</div>
+			);
+		} else {
+			return null;
+		}
 	});
-	return <div className="ninja-list">{ninjaList}</div>;
+    */
+	return (
+		<div className="ninja-list">
+			{ninjas.map(ninja => {
+				return ninja.age > 20 ? (
+					<div className="ninja" key={ninja.id}>
+						<div>Name: {ninja.name}</div>
+						<div>Age: {ninja.age}</div>
+						<div>Belt: {ninja.belt}</div>
+					</div>
+				) : null; //return condition ? (return if true): (return if false)
+			})}
+		</div>
+	);
 };
 
 export default Ninjas;
